2738966ccd7b301d57d1a02cd7c6ed6e1dc57f32
snehabaviskar22 <snehabaviskar025@gmail.com>
2024-08-28 12:55:02 +0530

    Add files via upload

---
+++ Ass1.java
@@ -0,0 +1,19 @@
+package java_assignment;
+
+import java.util.Scanner;
+
+public class Ass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter two numbers :");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		sum=(num1-num2*(num1/num2));
+		System.out.println("Remainder is of "+num1+" is "+sum);
+		sc.close();
+	}
+
+}
